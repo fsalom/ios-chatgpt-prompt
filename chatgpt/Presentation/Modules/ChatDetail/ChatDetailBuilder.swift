558dc307a6bc043d7ec1d17be40e8cc5464757c6
Fernando Salom <fer@rudo.es>
2023-05-07 00:15:31 +0200

    Add Chat detail

---
+++ chatgpt/Presentation/Modules/ChatDetail/ChatDetailBuilder.swift
@@ -0,0 +1,15 @@
+//
+//  ChatDetailBuilder.swift
+//  chatgpt
+//
+//  Created by Fernando Salom Carratala on 6/5/23.
+//
+
+import Foundation
+
+class ChatDetailBuilder {
+    func build() -> ChatDetailView {
+        let view = ChatDetailView()
+        return view
+    }
+}
---
+++ chatgpt/Presentation/Modules/ChatDetail/ChatDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatDetailView.swift
+//  chatgpt
+//
+//  Created by Fernando Salom Carratala on 6/5/23.
+//
+
+import SwiftUI
+
+struct ChatDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatDetailView()
+    }
+}
